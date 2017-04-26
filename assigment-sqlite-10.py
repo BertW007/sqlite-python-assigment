7a98ffdf53fd414c002350d777c5647691cedcc4
Lidia Vasileva <lydia.r.vasileva@gmail.com>
2017-04-26 13:05:45 +0200

    delete test-gui

--- assigment-sqlite-10.py
+++ assigment-sqlite-10.py
@@ -23,23 +23,23 @@ class Gui:
 		self.name = tkinter.StringVar()
 
 		#Sets the label\entry widgets - ID
-		self.id_label = tkinter.Label(self.top_frame, text='ID: ').grid(row=0,column=0, sticky=W)
+		self.id_label = tkinter.Label(self.top_frame, text='ID: ').grid(row=0, column=0, sticky=W)
 		self.id_entry = tkinter.Entry(self.top_frame, textvariable = self.id, width = 20).grid(row=0,column=1)
 
 		#Sets the label\entry widgets - NAME
-		self.name_label = tkinter.Label(self.top_frame, text='Name: ').grid(row=1,column=0, sticky=W)
+		self.name_label = tkinter.Label(self.top_frame, text='Name: ').grid(row=1, column=0, sticky=W)
 		self.name_entry = tkinter.Entry(self.top_frame, textvariable = self.name, width = 20).grid(row=1,column=1)
 
 		#Sets the label\entry widgets - E-MAIL
-		self.email_name = tkinter.Label(self.top_frame, text='E-mail: ').grid(row=2,column=0, sticky=W)
+		self.email_name = tkinter.Label(self.top_frame, text='E-mail: ').grid(row=2, column=0, sticky=W)
 		self.email_entry = tkinter.Entry(self.top_frame, textvariable = self.id, width = 20).grid(row=2,column=1)
 
 		#Sets the label\entry widgets - ADDRESS
-		self.address_label = tkinter.Label(self.top_frame, text='Address: ').grid(row=3,column=0, sticky=W)
+		self.address_label = tkinter.Label(self.top_frame, text='Address: ').grid(row=3, column=0, sticky=W)
 		self.address_entry = tkinter.Entry(self.top_frame, textvariable = self.id, width = 20).grid(row=3,column=1)
 
 		#Sets the label\entry widgets - CITY
-		self.city_label = tkinter.Label(self.top_frame, text='City: ').grid(row=4,column=0, sticky=W)
+		self.city_label = tkinter.Label(self.top_frame, text='City: ').grid(row=4, column=0, sticky=W)
 		self.city_entry = tkinter.Entry(self.top_frame, textvariable = self.id, width = 20).grid(row=4,column=1)
 
 		#Sets the text widget - DISPLAY DATA
@@ -51,9 +51,9 @@ class Gui:
 		self.text_window['yscrollcommand'] = self.scrollb.set
 
 		#Sets the button widgets
-		self.show_button = tkinter.Button(self.bottom_frame, text='Show', command=self.main_window.destroy).grid(row=0,column=0, pady=10)
-		self.help_button = tkinter.Button(self.bottom_frame, text='Help', command=self.main_window.destroy).grid(row=0,column=1, pady=10)
-		self.quit_button = tkinter.Button(self.bottom_frame, text='Quit', command=self.main_window.destroy).grid(row=0,column=2, pady=10)
+		self.show_button = tkinter.Button(self.bottom_frame, text='Show', command=self.main_window.destroy).grid(row=0, column=0, pady=10)
+		self.help_button = tkinter.Button(self.bottom_frame, text='Help', command=self.main_window.destroy).grid(row=0, column=1, pady=10)
+		self.quit_button = tkinter.Button(self.bottom_frame, text='Quit', command=self.main_window.destroy).grid(row=0, column=2, pady=10)
 
 		#Packing the frames
 		self.top_frame.pack()
